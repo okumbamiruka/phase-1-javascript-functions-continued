cc6fc7b4b9f21632a035a83d5206ec29665582c2
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-20 12:54:20 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const mondayWork = (activity = 'go to the office') => {
+    return `This Monday, I will ${activity}.`
+}
+
+const saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+console.log(wrapAdjective('*')('a dedicated programmer'))
